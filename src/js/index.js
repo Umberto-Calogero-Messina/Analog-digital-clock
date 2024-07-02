9759f4aa5b1ad1b8acfd6e9785a91914334b4d13
Genezis <ucmessina@alumnostrazos.net>
2024-07-02 10:05:18 +0200

    +Final Work

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import '../scss/styles.scss';
+const rootStyles = document.documentElement.style;
+
 const hoursElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 const dayOfWeekElement = document.getElementById('day-of-week');
@@ -6,6 +8,7 @@ const dayElement = document.getElementById('day');
 const monthElement = document.getElementById('month');
 const yearElement = document.getElementById('year');
 
+const fixTimeZero = number => (number < 10 ? '0' + number : number);
 const dayWeekObject = ['Sunday', 'Monday', 'Tusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const monthObject = [
   'January',
@@ -32,12 +35,22 @@ const setClockTime = () => {
   const currentMonth = realTime.getMonth();
   const currentYear = realTime.getFullYear();
 
-  hoursElement.textContent = hours;
-  minutesElement.textContent = minutes;
+  //digital Clock
+  hoursElement.textContent = fixTimeZero(hours);
+  minutesElement.textContent = fixTimeZero(minutes);
   dayOfWeekElement.textContent = dayWeekObject[today];
   dayElement.textContent = date;
   monthElement.textContent = monthObject[currentMonth];
   yearElement.textContent = currentYear;
+
+  //analogic Clock
+  const getAnalogicSecond = (360 / 60) * seconds;
+  const getAnalogicMinutes = (360 / 60) * minutes;
+  const getAnalogicHour = (360 / 12) * hours;
+
+  rootStyles.setProperty('--rotate-sec', getAnalogicSecond + 'deg');
+  rootStyles.setProperty('--rotate-min', getAnalogicMinutes + 'deg');
+  rootStyles.setProperty('--rotate-hour', getAnalogicHour + 'deg');
 };
 
 setInterval(() => {
